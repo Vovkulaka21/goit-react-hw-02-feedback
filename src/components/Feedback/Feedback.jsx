2f03d3f596d087f3634538520b72ce809d92d3f4
Vovkulaka21 <Vovkulaka16@gmail.com>
2023-12-30 00:32:26 +0200

    buttons

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,21 +3,26 @@
 import { Component } from 'react';
 
 class Feedback extends Component {
-
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
-  }
+    bad: 0,
+  };
 
   render() {
     return (
       <div>
         <h1>Please Leave Feedback</h1>
         <ul>
-          <li>Good</li>
-          <li>Netural</li>
-          <li>Bad</li>
+          <li>
+            <a href="#">Good</a>
+          </li>
+          <li>
+            <a href="#">Netural</a>
+          </li>
+          <li>
+            <a href="#">Bad</a>
+          </li>
         </ul>
         <h2>Statistics</h2>
         <ul>
