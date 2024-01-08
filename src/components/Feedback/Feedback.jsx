55a19d003eb650616db2a22177591b5944611570
Vovkulaka21 <Vovkulaka16@gmail.com>
2024-01-08 23:52:52 +0200

    Functional

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,34 +3,72 @@ import css from './Feedback.module.css';
 import { Component } from 'react';
 
 class Feedback extends Component {
+  static feedbackOptions = ['good', 'neutral', 'bad'];
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+
+    const total = good + neutral + bad;
+
+    return total;
+  }
+
+  countPositiveFeedbackPercentage(goodfeedback) {
+    const total = this.countTotalFeedback();
+
+    if (!total) {
+      return 0;
+    }
+
+    const procent = (total * goodfeedback) / 100;
+
+    return procent;
+  }
+
+  yourFeedback(reaction) {
+    this.setState(prevState => {
+      return {
+        [reaction]: prevState[reaction] + 1,
+      };
+    });
+  }
+
   render() {
+    const { good, neutral, bad } = this.state;
+
+    const optionButton = Feedback.feedbackOptions.map(option => (
+      <button
+        onClick={() => this.yourFeedback(option)}
+        key={option}
+        className={css.button}
+      >
+        {option}
+      </button>
+    ));
+
+    const procent = this.countPositiveFeedbackPercentage(good);
+
+    const total = this.countTotalFeedback();
+
     return (
-      <div>
-        <h1>Please Leave Feedback</h1>
-        <ul className={css.list}>
-          <li>
-            <button type='submit' className={css.button}>Good</button>
-          </li>
-          <li>
-            <button type='submit' className={css.button}>Netural</button>
-          </li>
-          <li>
-            <button type='submit' className={css.button}>Bad</button>
-          </li>
-        </ul>
-        <h2>Statistics</h2>
+      <div className={css.box}>
+        <h1 className={css.feedbacktitle}>Please Leave Feedback</h1>
+        <div className={css.feedbackblock}>{optionButton}</div>
+
+        <h2 className={css.titlestat}>Statistics</h2>
         <ul className={css.resultlist}>
-          <li>Good</li>
-          <li>Netural</li>
-          <li>Bad</li>
+          <li>Good: {good}</li>
+          <li>Netural: {neutral}</li>
+          <li>Bad: {bad}</li>
+          <li>Total: {total}</li>
         </ul>
-        <h3 className={css.result}>Positive Feedback %</h3>
+        <p className={css.result}>Positive Feedback {procent} %</p>
       </div>
     );
   }
