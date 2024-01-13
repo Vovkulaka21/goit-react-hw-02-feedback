41f6872e9e9323f293d85e294b9a48efe8039866
Vovkulaka21 <Vovkulaka16@gmail.com>
2024-01-13 03:11:55 +0200

    Impot into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,71 @@
-import Feedback from './Feedback/Feedback';
+import { Component } from 'react';
+
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+
+import FeedbackStat from 'components/FeedbackStat/FeedbackStat';
+
+import css from "./App.module.css"
 
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
+export class App extends Component {
+
+  static feedbackOptions = ['good', 'neutral', 'bad'];
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+
+    const total = good + neutral + bad;
+
+    return total;
+  }
+
+  countPositiveFeedbackPercentage(goodfeedback) {
+    const total = this.countTotalFeedback();
+
+    if (!total) {
+      return 0;
+    }
+
+    const procent = (goodfeedback * 100) / total;
+
+    return Math.round(Number(procent).toFixed(2));
+  }
+
+  yourFeedback = reaction => {
+    this.setState(prevState => {
+      return {
+        [reaction]: prevState[reaction] + 1,
+      };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    const procent = this.countPositiveFeedbackPercentage(good);
+
+    const total = this.countTotalFeedback();
+
+    return (
+      <div className={css.box}>
+        <FeedbackOptions
+          options={App.feedbackOptions}
+          onLeaveFeedback={this.yourFeedback}
+        />
+        <FeedbackStat
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={procent}
+        />
+      </div>
+    );
+  } 
 };
